30f2acee82b12bae1917ffb04154a08a106333cc
sirah <mohcine.sirah@gmail.com>
2025-10-01 11:46:46 +0200

    Update test_transformations.py

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,1 +1,14 @@
 
+import pytest
+from pyspark.sql import SparkSession
+from libs.transformations import clean_sales
+
+spark = SparkSession.builder.master("local[1]").appName("tests").getOrCreate()
+
+def test_clean_sales():
+    df = spark.createDataFrame(
+        [(1, "Paris"), (1, "Paris"), (2, None)],
+        ["id", "city"]
+    )
+    df_clean = clean_sales(df)
+    assert df_clean.count() == 1  # Suppression doublon + null
