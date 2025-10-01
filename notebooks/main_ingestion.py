e70194124411579cc13dc95e0931a55f73de290c
sirah <mohcine.sirah@gmail.com>
2025-10-01 11:46:27 +0200

    Update main_ingestion.py

--- notebooks/main_ingestion.py
+++ notebooks/main_ingestion.py
@@ -1,1 +1,23 @@
 
+import json
+from libs.io_utils import write_delta
+from libs.transformations import clean_sales
+
+# Paramètre d'environnement
+env = dbutils.widgets.get("env")
+
+# Charger la config JSON
+with open(f"/dbfs/FileStore/config/{env}.json") as f:
+    config = json.load(f)
+
+input_path = config["input_path"]
+output_path = config["output_path"]
+
+# Lire les données brutes
+df = spark.read.csv(input_path, header=True)
+
+# Transformer
+df_clean = clean_sales(df)
+
+# Sauvegarder en Delta
+write_delta(df_clean, output_path)
