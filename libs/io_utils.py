ebc8a4adcd3a9450aa302de3454ab868993b4da7
sirah <mohcine.sirah@gmail.com>
2025-10-01 11:40:42 +0200

    Update io_utils.py

--- libs/io_utils.py
+++ libs/io_utils.py
@@ -1,1 +1,7 @@
+from pyspark.sql import SparkSession
 
+def read_delta(spark: SparkSession, path: str):
+    return spark.read.format("delta").load(path)
+
+def write_delta(df, path: str, mode="overwrite"):
+    df.write.format("delta").mode(mode).save(path)
